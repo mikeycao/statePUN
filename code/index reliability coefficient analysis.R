ecab191b4f7330776a28e92cbcdcfd27dc917052
Michael Cao <mlc105@duke.edu>
2026-01-17 13:36:22 -0500

    Update index reliability coefficient analysis.R

--- code/index reliability coefficient analysis.R
+++ code/index reliability coefficient analysis.R
@@ -5,14 +5,15 @@
 library(psych)
 
 #calculating Cronbach's alpha for the poverty index 
-df <- read.csv(file.choose()) #select the .csv file "combined_poverty_index_2002-2018.csv"
+df <- read.csv(file.choose()) #select the .csv file "combined_poverty_index_2000-2018.csv"
 povindex <- df[, c("z1", "z2", "z3", "z4", "z5")]
 alpha_result <- alpha(povindex, discrete = FALSE)
 alpha_result$total
 
 #calculating Cronbach's alpha for the population heterogeneity index 
-df <- read.csv(file.choose()) #select the .csv file "urban_added_combined_population_heterogeneity_index_2002-2018.csv"
+df <- read.csv(file.choose()) #select the .csv file "combined_population_heterogeneity_index_with_urban_2000-2018.csv"
 pophetindex <- df[, c("z1", "z2", "z3")]
 alpha_result <- alpha(pophetindex, discrete = FALSE)
 alpha_result$total
+
 
